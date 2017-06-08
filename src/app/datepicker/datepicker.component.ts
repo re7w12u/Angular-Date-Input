da9868d3edc595218c61f9a03609e8fc3202bd3c
Julien <julien.bessiere@non.schneider-electric.com>
2017-06-08 15:25:15 +0200

    parse datepicker result as french date using $.datepicker.parseDate magic !

--- src/app/datepicker/datepicker.component.ts
+++ src/app/datepicker/datepicker.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Output, Input, EventEmitter } from '@angular/core';
+import * as moment from 'moment';
 
 declare var $: any;
 
@@ -22,11 +23,24 @@ export class DatepickerComponent implements OnInit {
       $(this.input.nativeElement).datepicker("option", $.datepicker.regional[ "fr" ]);
         $(this.input.nativeElement).datepicker({
             onSelect: (d) => {
-                this.date = new Date(d);
+                this.date = $.datepicker.parseDate('dd/mm/yy',d);
                 this.date.setHours(this.date.getHours() + 2);
                 this.dateChange.emit(this.date);
             }
         });
+    }
+
+    parseDate(input, format?) {
+      format = format || 'mm/dd/yyyy'; // default format
+      var parts = input.match(/(\d+)/g), 
+      i = 0, fmt = {};
+      // extract date-part indexes from the format
+      format.replace(/(yyyy|dd|mm)/g, function(part) {
+         console.table(fmt);
+         fmt[part] = i++; 
+        });
+
+      return new Date(parts[fmt['yyyy']], parts[fmt['mm']]-1, parts[fmt['dd']]);
     }
 
 }
